7866125a0e85f2193c6c6e16925e85e78d2ae591
Adam Kiss <iam@adamkiss.com>
2017-03-20 20:21:49 +0100

    Repeat runner

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/usr/local/bin/zsh
+
+
+echo
+echo
+echo "TESTING 1000 files"
+time ./test.php json 1000
+time ./test.php pw-json 1000
+time ./test.php spyc 1000
+time ./test.php symf 1000
+time ./test.php radham 1000
+
+
+echo
+echo
+echo "TESTING 2000 files"
+time ./test.php json 2000
+time ./test.php pw-json 2000
+time ./test.php spyc 2000
+time ./test.php symf 2000
+time ./test.php radham 2000
+
+echo
+echo
+echo "TESTING 5000 files"
+time ./test.php json 5000
+time ./test.php pw-json 5000
+time ./test.php spyc 5000
+time ./test.php symf 5000
+time ./test.php radham 5000
